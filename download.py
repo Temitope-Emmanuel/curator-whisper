d4852cfa5c23df15c97158123ab712b380c923bb
Temitope-Emmanuel <temitopeojo0@gmail.com>
2023-02-14 11:30:24 +0000

    first commit

--- download.py
+++ download.py
@@ -1,13 +1,14 @@
 # In this file, we define download_model
 # It runs during container build time to get model weights built into the container
 
-# In this example: A Huggingface BERT model
+import torch
+import whisper
 
-from transformers import pipeline
 
 def download_model():
-    # do a dry run of loading the huggingface model, which will download weights
-    pipeline('fill-mask', model='bert-base-uncased')
-
+    #medium, large-v1, large-v2
+    model_name = "medium"
+    model = whisper.load_model(model_name)
+    
 if __name__ == "__main__":
     download_model()
